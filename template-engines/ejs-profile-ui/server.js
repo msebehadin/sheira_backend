47e47795ba092d758b70f1807c231461189d9a07
Mohammed Sebehadin <mohammedsebehadin07@gmail.com>
2026-01-20 11:26:02 +0300

    feat: set up EJS view engine and add initial profile UI template

--- template-engines/ejs-profile-ui/server.js
+++ template-engines/ejs-profile-ui/server.js
@@ -1,5 +1,6 @@
 import express from 'express'
 const app=express();
+app.set('view engine','ejs')
 app.get('/',(req,res)=>{
     const user={
   name: "Fatima Noor",
@@ -7,6 +8,7 @@ app.get('/',(req,res)=>{
   profession: "Software Developer",
   skills: ["Node.js", "Express", "MongoDB", "EJS"]
 }
+res.render('index',{user})
 })
 app.listen(400,()=>{
     console.log('the ejs is running')
